718aace6c878974d1a1812e62ce72e561ebaadd7
codepath <chen836027055@gmail.com>
2021-08-02 04:11:50 -0400

    Adding app/src/main/java/com/example/todoprework/ItemsAdapter.java

--- app/src/main/java/com/example/todoprework/ItemsAdapter.java
+++ app/src/main/java/com/example/todoprework/ItemsAdapter.java
@@ -10,43 +10,58 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+//responsible for displaying data from the model into a row in the recycler view
 public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
 
+    //to pass longClick information from main activity into adapater
     public interface OnLongClickListener {
+        //the main class will implement this method, and it
+        //need to know the position where the long-press happen
         void  onItemLongClicked(int position);
+    }
+
+    //we want to define a similar interface which will communicate back to
+    //main activity and inform us what position was tapped
+    public interface OnClickListener{
+        void onItemClicked(int position);
     }
 
     List<String> items;
     OnLongClickListener longClickListener;
+    OnClickListener clickListener;
 
-
-    public ItemsAdapter(List<String> items, OnLongClickListener longClickListener){
+    //constructor
+    public ItemsAdapter(List<String> items, OnLongClickListener longClickListener, OnClickListener clickListener){
         this.items = items;
         this.longClickListener = longClickListener;
+        this.clickListener = clickListener;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-
+        //use layout inflator to inflate a view
         View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1,parent,false);
-
+        //wrap it inside a view holder and return it
         return new ViewHolder(todoView);
     }
 
     @Override
-    public  void onBindViewHolder(ViewHolder holder, int position){
-
+    //responsible for binding data to particular view holder
+    public void onBindViewHolder(ViewHolder holder, int position){
+        //grab the item at the position
         String item = items.get(position);
-
+        //bind the items into the specified view holder
         holder.bind(item);
     }
 
     @Override
+    //tells the RV how many items are in the list
     public int getItemCount(){
         return items.size();
     }
 
+    //Container to provide easy access to views that represent each row of the list
     class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView tvItem;
@@ -58,9 +73,18 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> 
 
         public void bind(String item){
             tvItem.setText(item);
+            tvItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    //when android notifies us that the textview has been tapped
+                    //we can invoke the method on out interface, and pass the position
+                    clickListener.onItemClicked(getAdapterPosition());
+                }
+            });
             tvItem.setOnLongClickListener(new View.OnLongClickListener(){
                 @Override
                 public boolean onLongClick(View v){
+                    //Notify the listener which position was long pressed.
                     longClickListener.onItemLongClicked(getAdapterPosition());
                     return true;
                 }
